ef4341626bfffd665684e12c1aaa7e8034605617
mirkamol <mirkamolasrorov5@gmail.com>
2023-10-11 16:38:59 +0500

    added CustomerTransactions class

--- src/main/java/org/example/model/CustomerTransactions.java
+++ src/main/java/org/example/model/CustomerTransactions.java
@@ -1,2 +1,15 @@
-package org.example.model;public class CustomerTransactions {
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomerTransactions {
+    private Integer transaction_id;
+    private Double amount;
+    private Customer customer;
+
 }
