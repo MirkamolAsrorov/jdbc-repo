c44a749e23030c4bd26c9ed7a6e55efac4b37472
mirkamol <mirkamolasrorov5@gmail.com>
2023-10-11 14:48:55 +0500

    added a Customer class

--- src/main/java/org/example/model/Customer.java
+++ src/main/java/org/example/model/Customer.java
@@ -1,2 +1,19 @@
-package org.example.model;public class Customer {
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Customer {
+    private String customer_id;
+    private String first_name;
+    private String last_name;
+
+    public Customer(String first_name, String last_name) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
 }
