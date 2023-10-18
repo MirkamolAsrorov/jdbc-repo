982e98376e7045f05a3d8d47556120f63d440df3
mirkamol <mirkamolasrorov5@gmail.com>
2023-10-18 16:54:11 +0500

    added Customer class

--- src/main/java/org/example/model/Customer.java
+++ src/main/java/org/example/model/Customer.java
@@ -8,12 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class Customer {
-    private String customer_id;
+    private Long customer_id;
     private String first_name;
     private String last_name;
 
-    public Customer(String first_name, String last_name) {
-        this.first_name = first_name;
-        this.last_name = last_name;
-    }
+
 }
